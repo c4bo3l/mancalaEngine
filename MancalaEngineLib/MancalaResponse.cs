925e6f1c9e452d1e2e414e9963795c10fe6f8ab5
c4bo3l <c4bo3l@gmail.com>
2019-07-09 00:50:21 +0800

    Initial commit.

---
+++ MancalaEngineLib/MancalaResponse.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// Author: Andrianto Dwi Laksono
+/// Date: July 9 2019
+/// </summary>
+namespace MancalaEngineLib
+{
+    public enum GameStatus
+    {
+        GameOn,
+        Player1Win,
+        Player2Win,
+        Draw
+    }
+
+    public struct MancalaResponse
+    {
+        public int[] Board;
+        public GameStatus GameStatus;
+        public int PlayerTurn;
+    }
+}
